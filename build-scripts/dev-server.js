4ae9e5bbd278200a1a9a5440ba2aaa6acc1fdf37
bornehill <ali.escamilla.navarro@gmail.com>
2018-08-23 23:54:24 -0500

    api service & mock data

--- build-scripts/dev-server.js
+++ build-scripts/dev-server.js
@@ -20,6 +20,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id":1, "firstName":"Borne", "lastName":"Hill", "email":"bornehill@hotmail.com"},
+    {"id":2, "firstName":"Doroteo", "lastName":"Arango", "email":"doroteo.arango@hotmail.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
---
+++ build-scripts/generate-mock-data.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+import jsf from 'json-schema-faker';
+import { schema } from './mock-data-schema';
+import fs from 'fs';
+import chalk from 'chalk';
+
+const json = JSON.stringify(jsf(schema));
+
+fs.writeFile("./src/api/db.json", json, (err) => {
+  if(err)
+  {
+    return console.log(chalk.red(err));
+  }
+
+  console.log(chalk.green("Mock data generated!!"));
+});
